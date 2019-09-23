48c211894fcd6da143728a50a88cd6bbe372d4a0
Searge <ashikcygan@gmail.com>
2019-09-23 09:25:03 +0300

    RegEx Teachers Solution

---
+++ w_1/regexp_sample/regexp_teachers.py
@@ -0,0 +1,15 @@
+# Решение получается довольно простым,
+# нужно только удобно разбить на группы каждую формулу:
+
+
+def calculate(data, findall):
+    matches = findall(r"([abc])([+-]?)=([abc])?([+-]?\d+)?")
+    for a, sign, b, number in matches:
+        right = data.get(b, 0) + int(number or 0)
+        if sign == "-":
+            data[a] -= right
+        elif sign == "+":
+            data[a] += right
+        else:
+            data[a] = right
+    return data
