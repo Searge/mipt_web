189efee9b525481ba090da275784527be1d12ae7
Searge <ashikcygan@gmail.com>
2019-09-23 09:23:42 +0300

    RegEx Solution

--- w_1/regexp_sample/regexp.py
+++ w_1/regexp_sample/regexp.py
@@ -1,7 +1,12 @@
 def calculate(data, findall):
-    matches = findall(r"")  # Если придумать хорошую регулярку, будет просто
-    for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
-        # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
-        data[v1] = data.get(v2, 0) + int(n or 0)
+    matches = findall(r"([abc])([+-]?)=([abc]?)([+-]?\d*)")
+    # Если придумать хорошую регулярку, будет просто
+    for v1, s, v2, n in matches:
+        if not s:
+            data[v1] = data.get(v2, 0) + int(n or 0)
+        elif (s == '+'):
+            data[v1] = data[v1] + data.get(v2, 0) + int(n or 0)
+        elif (s == '-'):
+            data[v1] = data[v1] - data.get(v2, 0) - int(n or 0)
 
     return data
