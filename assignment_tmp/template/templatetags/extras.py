1361b67ce8fdf2b06b31d25402d86384b5bb1c5c
Searge <ashikcygan@gmail.com>
2019-10-10 17:19:09 +0300

    4th week | Templates filters

--- assignment_tmp/template/templatetags/extras.py
+++ assignment_tmp/template/templatetags/extras.py
@@ -1,1 +1,26 @@
 from django import template
+
+register = template.Library()
+
+
+@register.filter(name='inc')
+def get_sum_of_args(a, b):
+    try:
+        a = int(a)
+        b = int(b)
+        return a + b
+    except:
+        return ''
+
+
+@register.simple_tag(name='division')
+def devide(a, b, to_int=False):
+    try:
+        a = int(a)
+        b = int(b)
+        if to_int == False:
+            return a / b
+        else:
+            return a // b
+    except:
+        return ''
