ab2a94109912840fba88099a1bd6c26b86c8660b
Searge <ashikcygan@gmail.com>
2019-10-16 11:09:54 +0300

    More natural blog

--- coursera_blog/core/views.py
+++ coursera_blog/core/views.py
@@ -1,12 +1,33 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
+from core.models import Topic, Category
+from django.db.models import Count
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    topics = Topic.objects.all().annotate(Count('categories'))
+    categories = Category.objects.all()
+
+    q = request.GET.get('q')
+    if q is not None:
+        topics = topics.filter(title__icontains=q)
+    category_pk = request.GET.get('category')
+    if category_pk is not None:
+        topics = topics.filter(category_pk=category_pk)
+
+    return render(request, 'core/index.html', context={
+        'topics': topics,
+        'categories': categories
+    })
 
 
 def topic_details(request, pk):
-    return render(request, 'core/topic_details.html')
+    try:
+        topic = Topic.objects.get(pk=pk)
+    except Topic.DoesNotExist:
+        raise Http404
+    return render(request, 'core/topic_details.html', context={
+        'topic': topic
+    })
