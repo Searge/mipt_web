32f3c0c4cd650e4cc1e2ba744a08dca13a6bfe52
Searge <ashikcygan@gmail.com>
2019-10-18 10:53:32 +0300

    Simple Forms Validations

--- w_5/coursera_forms/formdummy/forms.py
+++ w_5/coursera_forms/formdummy/forms.py
@@ -5,3 +5,7 @@ class DummyForm(forms.Form):
     text = forms.CharField(label='Отзыв', min_length=3, max_length=10)
     grade = forms.IntegerField(label='Оценка', min_value=1, max_value=100)
     image = forms.FileField(label='Фото', required=False)
+
+    def clean_text(self):
+        if 'abc' not in self.cleaned_data['text']:
+            raise forms.ValidationError('Не о том вы пишете')
