b30cdf563dc6ba3fb7836567184e641ab890c8e8
Searge <ashikcygan@gmail.com>
2019-10-18 09:47:01 +0300

    POST method

--- w_5/coursera_forms/formdummy/views.py
+++ w_5/coursera_forms/formdummy/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
 from django.views import View
 
 
 class FormDummyView(View):
 
     def get(self, request):
-        hello = request.GET.get('hello')
-        return render(request, 'form.html', {'hello': hello})
+        return render(request, 'form.html', {})
+
+    def post(self, request):
+        text = request.POST.get('text')
+        grade = request.POST.get('grade')
+        image = request.FILES.get('image')
+        content = image.read()
+        context = {
+            'text': text,
+            'grade': grade,
+            'content': content
+        }
+        return render(request, 'form.html', context)
